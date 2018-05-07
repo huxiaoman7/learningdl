1dbe6f2b79fd08ab57e2ec432b8b9cdc08900be5
huxiaoman <charlotte77_hu@sina.com>
2018-05-07 13:27:01 +0800

    update sgd

---
+++ Chapter3/sgd/gradient_descent.py
@@ -0,0 +1,105 @@
+# USAGE
+# python gradient_descent.py
+
+# import the necessary packages
+#import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+import numpy as np
+import argparse
+
+def sigmoid_activation(x):
+	# compute and return the sigmoid activation value for a
+	# given input value
+	return 1.0 / (1 + np.exp(-x))
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--epochs", type=float, default=100,
+	help="# of epochs")
+ap.add_argument("-a", "--alpha", type=float, default=0.01,
+	help="learning rate")
+args = vars(ap.parse_args())
+
+# generate a 2-class classification problem with 250 data points,
+# where each data point is a 2D feature vector
+(X, y) = make_blobs(n_samples=250, n_features=2, centers=2,
+	cluster_std=1.05, random_state=20)
+
+# insert a column of 1's as the first entry in the feature
+# vector -- this is a little trick that allows us to treat
+# the bias as a trainable parameter *within* the weight matrix
+# rather than an entirely separate variable
+X = np.c_[np.ones((X.shape[0])), X]
+
+# initialize our weight matrix such it has the same number of
+# columns as our input features
+print("[INFO] starting training...")
+W = np.random.uniform(size=(X.shape[1],))
+
+# initialize a list to store the loss value for each epoch
+lossHistory = []
+
+# loop over the desired number of epochs
+for epoch in np.arange(0, args["epochs"]):
+	# take the dot product between our features `X` and the
+	# weight matrix `W`, then pass this value through the
+	# sigmoid activation function, thereby giving us our
+	# predictions on the dataset
+	preds = sigmoid_activation(X.dot(W))
+
+	# now that we have our predictions, we need to determine
+	# our `error`, which is the difference between our predictions
+	# and the true values
+	error = preds - y
+
+	# given our `error`, we can compute the total loss value as
+	# the sum of squared loss -- ideally, our loss should
+	# decrease as we continue training
+	loss = np.sum(error ** 2)
+	lossHistory.append(loss)
+	print("[INFO] epoch #{}, loss={:.7f}".format(epoch + 1, loss))
+
+	# the gradient update is therefore the dot product between
+	# the transpose of `X` and our error, scaled by the total
+	# number of data points in `X`
+	gradient = X.T.dot(error) / X.shape[0]
+
+	# in the update stage, all we need to do is nudge our weight
+	# matrix in the opposite direction of the gradient (hence the
+	# term "gradient descent" by taking a small step towards a
+	# set of "more optimal" parameters
+	W += -args["alpha"] * gradient
+
+# to demonstrate how to use our weight matrix as a classifier,
+# let's look over our a sample of training examples
+for i in np.random.choice(250, 10):
+	# compute the prediction by taking the dot product of the
+	# current feature vector with the weight matrix W, then
+	# passing it through the sigmoid activation function
+	activation = sigmoid_activation(X[i].dot(W))
+
+	# the sigmoid function is defined over the range y=[0, 1],
+	# so we can use 0.5 as our threshold -- if `activation` is
+	# below 0.5, it's class `0`; otherwise it's class `1`
+	label = 0 if activation < 0.5 else 1
+
+	# show our output classification
+	print("activation={:.4f}; predicted_label={}, true_label={}".format(
+		activation, label, y[i]))
+
+# compute the line of best fit by setting the sigmoid function
+# to 0 and solving for X2 in terms of X1
+Y = (-W[0] - (W[1] * X)) / W[2]
+
+# plot the original data along with our line of best fit
+#plt.figure()
+#plt.scatter(X[:, 1], X[:, 2], marker="o", c=y)
+#plt.plot(X, Y, "r-")
+
+# construct a figure that plots the loss over time
+#fig = plt.figure()
+#plt.plot(np.arange(0, args["epochs"]), lossHistory)
+#fig.suptitle("Training Loss")
+#plt.xlabel("Epoch #")
+#plt.ylabel("Loss")
+#plt.show()
---
+++ Chapter3/sgd/sgd.py
@@ -0,0 +1,78 @@
+# USAGE
+# python gradient_descent.py
+
+# import the necessary packages
+#import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+import numpy as np
+import argparse
+
+def sigmoid_activation(x):
+	return 1.0 / (1 + np.exp(-x))
+
+def next_training_batch(X, y, batchSize):
+    for i in np.arange(0, X.shape[0], batchSize):
+        yield (X[i:i + batchSize], y[i:i + batchSize])
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--epochs", type=float, default=100,
+	help="# of epochs")
+ap.add_argument("-a", "--alpha", type=float, default=0.01,
+	help="learning rate")
+ap.add_argument("-b", "--batch-size", type=int, default=32,
+    help="size of SGD mini-batches")
+args = vars(ap.parse_args())
+
+(X, y) = make_blobs(n_samples=400, n_features=2, centers=2,
+    cluster_std=2.5, random_state=95)
+X = np.c_[np.ones((X.shape[0])), X]
+
+print("[INFO] starting training...")
+W = np.random.uniform(size=(X.shape[1],))
+
+lossHistory = []
+
+
+for epoch in np.arange(0, args["epochs"]):
+    epochLoss = []
+ 
+    for (batchX, batchY) in next_training_batch(X, y, args["batch_size"]):
+
+        preds = sigmoid_activation(batchX.dot(W))
+ 
+
+        error = preds - batchY
+ 
+
+        loss = np.sum(error ** 2)
+        epochLoss.append(loss)
+ 
+
+        gradient = batchX.T.dot(error) / batchX.shape[0]
+ 
+
+        W += -args["alpha"] * gradient
+ 
+
+    lossHistory.append(np.average(epochLoss))
+    print("[INFO] epoch #{}, loss={:.7f}".format(epoch + 1, loss))
+
+for i in np.random.choice(250, 10):
+	activation = sigmoid_activation(X[i].dot(W))
+
+	label = 0 if activation < 0.5 else 1
+
+	# show our output classification
+	print("activation={:.4f}; predicted_label={}, true_label={}".format(
+		activation, label, y[i]))
+
+#plt.figure()
+#plt.scatter(X[:, 1], X[:, 2], marker="o", c=y)
+#plt.plot(X, Y, "r-")
+
+#fig = plt.figure()
+#plt.plot(np.arange(0, args["epochs"]), lossHistory)
+#fig.suptitle("Training Loss")
+#plt.xlabel("Epoch #")
+#plt.ylabel("Loss")
+#plt.show()
